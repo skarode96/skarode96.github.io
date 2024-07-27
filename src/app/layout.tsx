978b059f90e9930e45aa8f696e02be19333643a6
SK <skarode96@gmail.com>
2024-07-28 00:25:21 +0100

    add title and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -54,20 +55,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+      <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta property="og:title" content="Sameer Karode" key="title"/>
+      </Head>
       <body
-        className={cn(
-          "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
-          fontSans.variable
-        )}
+          className={cn(
+              "min-h-screen bg-background font-sans antialiased max-w-2xl mx-auto py-12 sm:py-24 px-6",
+              fontSans.variable
+          )}
       >
-        <ThemeProvider attribute="class" defaultTheme="dark">
-          <TooltipProvider delayDuration={0}>
-            {children}
-            <Navbar />
-          </TooltipProvider>
-        </ThemeProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <TooltipProvider delayDuration={0}>
+          {children}
+          <Navbar/>
+        </TooltipProvider>
+      </ThemeProvider>
       </body>
-    </html>
+      </html>
   );
 }
